1344d29d89ac1bdb7e70b506b03ded2eb7402712
Thomas Schlender <thebuoyant@gmail.com>
2026-04-03 19:04:48 +0200

    MTL-1 Add base password input

--- src/components/password-strength-meter/PasswordStrengthMeter.stories.tsx
+++ src/components/password-strength-meter/PasswordStrengthMeter.stories.tsx
@@ -1,37 +1,24 @@
 import type { Meta, StoryObj } from "@storybook/react-vite";
-import * as React from "react";
-import { Box, TextField, Typography } from "@mui/material";
+import { Box } from "@mui/material";
 import { PasswordStrengthMeter } from "./PasswordStrengthMeter";
 
 const meta: Meta<typeof PasswordStrengthMeter> = {
   title: "Components/PasswordStrengthMeter",
   component: PasswordStrengthMeter,
-  argTypes: {
-    password: { control: false },
-  },
+  argTypes: {},
 };
 export default meta;
 
 type Story = StoryObj<typeof PasswordStrengthMeter>;
 
 export const Default: Story = {
-  args: {},
-  render: (args, context) => {
-    const [pw, setPw] = React.useState("");
-
+  args: {
+    showPasswordVisibility: true,
+  },
+  render: (args) => {
     return (
       <Box sx={{ maxWidth: 420 }}>
-        <TextField
-          fullWidth
-          type="password"
-          label="Passwort"
-          value={pw}
-          onChange={(e) => setPw(e.target.value)}
-        />
-
-        <Box sx={{ mt: 2 }}>
-          <PasswordStrengthMeter {...args} password={pw} />
-        </Box>
+        <PasswordStrengthMeter {...args} />
       </Box>
     );
   },
--- src/components/password-strength-meter/PasswordStrengthMeter.tsx
+++ src/components/password-strength-meter/PasswordStrengthMeter.tsx
@@ -1,12 +1,82 @@
-import { Stack } from "@mui/material";
-import * as React from "react";
+import {
+  FormControl,
+  IconButton,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Stack,
+} from "@mui/material";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { useState } from "react";
 
 export type PasswordStrengthMeterProps = {
-  password: string;
+  showPasswordVisibility?: boolean;
 };
 
 export function PasswordStrengthMeter({
-  password,
+  showPasswordVisibility = true,
 }: PasswordStrengthMeterProps) {
-  return <Stack>{password}</Stack>;
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const handleClickShowPassword = () => {
+    setShowPassword((show) => !show);
+  };
+
+  const handleMouseDownPassword = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    event.preventDefault();
+  };
+
+  const handleMouseUpPassword = (
+    event: React.MouseEvent<HTMLButtonElement>,
+  ) => {
+    event.preventDefault();
+  };
+
+  const handleOnChange = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setPassword(event.target.value);
+  };
+
+  return (
+    <Stack>
+      <div className="password-input-wrapper">
+        <FormControl variant="outlined" fullWidth>
+          <InputLabel htmlFor="outlined-adornment-password">
+            Password
+          </InputLabel>
+
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={showPassword ? "text" : "password"}
+            fullWidth
+            value={password}
+            onChange={handleOnChange}
+            endAdornment={
+              showPasswordVisibility ? (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label={
+                      showPassword ? "Hide password" : "Show password"
+                    }
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                    onMouseUp={handleMouseUpPassword}
+                    edge="end"
+                  >
+                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                  </IconButton>
+                </InputAdornment>
+              ) : null
+            }
+            label="Password"
+          />
+        </FormControl>
+      </div>
+    </Stack>
+  );
 }
