117dbbf68c7b5524b21234066e9b95af47977a6e
Thomas Schlender <thebuoyant@gmail.com>
2026-05-18 19:24:47 +0200

    MTL-7 Fix storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,10 +2,16 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.@(ts|tsx)"],
-  addons: ["@storybook/addon-essentials"],
+  addons: [],
   framework: {
     name: "@storybook/react-vite",
     options: {},
+  },
+  viteFinal: async (config) => {
+    config.optimizeDeps ??= {};
+    config.optimizeDeps.include ??= [];
+    config.optimizeDeps.include.push("@mui/icons-material");
+    return config;
   },
 };
 
